b24d570e904e84d9cf6d17416df9f7519990196d
JAB47 <jbangie45@gmail.com>
2022-09-09 10:03:17 -0700

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,23 +1,23 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
- *main-beginning
- *
- *Description: Prints the size of different types of C to the terminal depending on the weather the system is a 32-bit or a 64-bit system
- *Return: (0) if a success
+ * main - Entry point
+ * Description- program that prints the size of various types
+ * Return: Always (0) (Success)
  */
 int main(void)
 {
-	int a  = sizeof(char);
-	int b = sizeof(int);
-	int c = sizeof(long int);
-	int d = sizeof(long long int);
-	int e = sizeof(float);
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+	
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 
-	printf("Size of a char: %d byte(s)\n", sizeof(a));
-	printf("Size of an int: %d byte(s0\n", sizeof(b));
-	printf("Size of a long int: %d byte(s)\n", sizeof(c));
-	printf("Size of a long long int: %d byte(s)\n", sizeof(d));
-	printf("Size of a float: %d byte(s)\n", sizeof(e));
 	return (0);
 }
