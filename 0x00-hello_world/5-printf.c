7de208fee9e2308f7b8082bb95031b747765465f
JAB47 <jbangie45@gmail.com>
2022-09-08 20:00:52 -0700

    Hello printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+/* Description dj */
+#include <stdio.n>
+/**
+ * main -Entry point
+ * Description: print with printf
+ * return: nothing
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
