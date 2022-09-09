ed97ac463c3dd3872f2fa288c337a5f31a9ac4b4
JAB47 <jbangie45@gmail.com>
2022-09-08 20:29:00 -0700

    Hello Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 /**
  * main - entery point
- * 
+ *
  * Return: Always 0 (Success)
  */
-int main(void) 
+int main(void)
 {
-	puts("\"Programing is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 		return (0);
 }
