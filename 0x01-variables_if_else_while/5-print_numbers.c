b250bb63aa043da0e7c9c5b7ecdc41ffc5f456c3
JAB47 <jbangie45@gmail.com>
2022-09-09 19:05:39 -0700

    NUMBERS

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main- beginning
+ * Description-Prints all decimal numbers
+ * Retrun: always 0 is sucess
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+	printf("\n");
+
+	return (0);
+}
