e75b42a9e555b904dcb6859fd4762602e8c4a1fd
JAB47 <jbangie45@gmail.com>
2022-09-09 18:56:07 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main- beginning
+ * Description- Selectively prints alphebets to screen
+ * Return: always 0 if success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97; i <=122; i++)
+	{
+		if(!(i == 113 || i == 101))
+			putchar(i);
+	}
+	putchar('\n');
+		return (0);
+}
