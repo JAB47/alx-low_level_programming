243309ff90cc53c7e9643c1d3a9c0d09391b58ff
JAB47 <jbangie45@gmail.com>
2022-09-09 19:41:51 -0700

    task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -11,9 +11,9 @@ int main(void)
 {
 	int i;
 
-	for (i = 97; i <=122; i++)
+	for (i = 97; i <= 122; i++)
 	{
-		if(!(i == 113 || i == 101))
+		if (!(i == 113 || i == 101))
 			putchar(i);
 	}
 	putchar('\n');
