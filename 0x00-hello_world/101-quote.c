2d74e665ff44be8f3723e9d9f5007129b21795cf
JAB47 <jbangie45@gmail.com>
2022-09-09 17:46:44 -0700

    task 6

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,12 @@
 #include <unistd.h>
 
 /**
- *main- Prints to standard error
- *Return: Always (0) on success
+ * main - Prints to the standard error
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
