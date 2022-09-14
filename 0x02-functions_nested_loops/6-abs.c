3b09d063a5338942531c7380d8604a7bf6ed0478
JAB47 <jbangie45@gmail.com>
2022-09-13 19:41:36 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _abs -The absolut value of an interger.
+ * @c: The number to be computed
+ *
+ * Retrun: Absolut value of number or 0
+ */
+int _abs(int c)
+{
+	if (c < 0)
+	{
+		int ans_val;
+
+		abs_val = c * -1;
+
+		return (abs_val);
+	}
+
+	return (0);
+}
+
