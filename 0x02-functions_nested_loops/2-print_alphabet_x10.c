9f3507d6302d2fcf45520cfd6414779eba2ab983
JAB47 <jbangie45@gmail.com>
2022-09-13 18:35:57 -0700

    x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,24 +3,18 @@
 /**
  * print_alphabest_x10 -prints alphabest 10 times
  *
- * Return:void
+ * Return: 0
  */
-
 void print_alphabet_x10(void)
 {
-	int round = 0;
-	char letter = 'a';
+	int a, b;
 
-	while (round < 10)
+	for (a = b; a <= 9; a++)
 	{
-		letter = 'a';
-		while (letter <= 'z')
+		for (b = 97; b <= 122; b++)
 		{
-			_putchar(letter);
-			letter++;
+			_putchar(b);
 		}
 		_putchar('\n');
-
-		round++;
 	}
 }
