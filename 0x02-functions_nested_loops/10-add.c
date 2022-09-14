6f956fa1c8c7ec3e5a3bdcca9e911e739a180935
JAB47 <jbangie45@gmail.com>
2022-09-13 20:19:19 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add -adds two values
+ * @n1: The 1st to be computed
+ * @n2: The 2nd to be computed
+ *
+ * Return: Interger value
+ */
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
+
