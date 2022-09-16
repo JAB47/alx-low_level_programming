aba976ebfbe2615240f7e7da74b1e5f1e6c14b0e
JAB47 <jbangie45@gmail.com>
2022-09-15 17:54:06 -0700

    task 1

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - infinite loop
+ *
+ * Return: always 0 on success
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("infinite loop incoming :(\n");
+
+	i = 0;
+/**
+ *      while (i < 10)
+ *              putchar(i);
+ */
+	printf("infinite loop avoided! \\o/\n");
+
+	return (0);
+}
