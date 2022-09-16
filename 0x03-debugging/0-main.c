6dd35cc60df3fba235d103587badc153a3513e19
JAB47 <jbangie45@gmail.com>
2022-09-15 17:30:11 -0700

    Multiple mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main: test function that prints positive or negative intergers
+ *
+ * Return: always 0 upon success
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
